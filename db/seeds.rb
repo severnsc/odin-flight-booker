cbc4a16b8dc5b793188c25beaad21b1f0aeb83e5
Chris Severns <severnsc@gmail.com>
2017-02-15 14:08:07 -0500

    Add flights controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,7 @@ Airport.create!(city: "Palm Beach", state: "FL", code: "PBI")
 Airport.create!(city: "Miami", state: "FL", code: "MIA")
 Airport.create!(city: "New York", state: "NY", code: "JFK")
 Airport.create!(city: "Boston", state: "MA", code: "BOS")
+
+30.times do |n|
+	Flight.create!(dept_airport_id: rand(1..20), arrival_airport_id: rand(1..20), dept_datetime: Time.zone.now, arrival_datetime: Time.zone.now + 2.hours, duration: 2.0)
+end
--- test/models/flight_test.rb
+++ test/models/flight_test.rb
@@ -1,7 +1,37 @@
 require 'test_helper'
 
 class FlightTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+ 
+	def setup
+		@flight = Flight.new(dept_airport: airports(:FLL), arrival_airport: airports(:MIA), dept_datetime: Time.zone.now, arrival_datetime: Time.zone.now + 2.hours, duration: 2.0)
+	end
+
+	test "should be valid" do
+		assert @flight.valid?
+	end
+
+	test "should have a dept airport" do
+		@flight.dept_airport_id = " "
+		assert_not @flight.valid?
+	end
+
+	test "should have an arrival airport" do
+		@flight.arrival_airport_id = " "
+		assert_not @flight.valid?
+	end
+
+	test "should have a dept_datetime" do
+		@flight.dept_datetime = " "
+		assert_not @flight.valid?
+	end
+
+	test "should have an arrival datetime" do
+		@flight.arrival_datetime = " "
+		assert_not @flight.valid?
+	end
+
+	test "should have a duration" do
+		@flight.duration = " "
+		assert_not @flight.valid?
+	end
 end
